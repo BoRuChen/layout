b70c7b2d70dd27b8223bf1736501e79991f73086
Boru-Chen <56582854+Boru-Chen@users.noreply.github.com>
2022-02-14 02:08:08 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter ,Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import {No005} from "./pages/no005";
 import {No001} from "./pages/no001";
 import {No002} from "./pages/no002";
@@ -9,15 +9,15 @@ import {No006} from "./pages/no006";
 
 function App() {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename={'layout'}>
           <Routes>
-              <Route exact path={'/'} element={<No001 />} />
-              <Route exact path={'/no002'} element={<No002 />} />
-              <Route exact path={'/no003'} element={<No003 />} />
-              <Route exact path={'/no004'} element={<No004 />} />
-              <Route exact path={'/no005'} element={<No005 />} />
-              <Route exact path={'/no006'} element={<No006 />} />
-              <Route exact path={'/no007'} element={<No007 />} />
+              <Route path={'/'} element={<No001 />} />
+              <Route path={'/no002'} element={<No002 />} />
+              <Route path={'/no003'} element={<No003 />} />
+              <Route path={'/no004'} element={<No004 />} />
+              <Route path={'/no005'} element={<No005 />} />
+              <Route path={'/no006'} element={<No006 />} />
+              <Route path={'/no007'} element={<No007 />} />
           </Routes>
       </BrowserRouter>
   );
